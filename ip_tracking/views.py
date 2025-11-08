077b06c2975366275f303984af9527b1a352bbcb
Ephraim Debel <ephraimdebel@gmail.com>
2025-11-08 12:05:31 +0300

    rate limiter added

--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from ratelimit.decorators import ratelimit
 
-# Create your views here.
+@csrf_exempt
+@ratelimit(key='ip', rate='5/m', method='POST', block=True)
+@ratelimit(key='user', rate='10/m', method='POST', block=True)
+def login_view(request):
+    """
+    A sensitive login view protected by rate limiting.
+    - Anonymous users: max 5 requests/min
+    - Authenticated users: max 10 requests/min
+    """
+    return JsonResponse({"message": "Login endpoint accessed"})
